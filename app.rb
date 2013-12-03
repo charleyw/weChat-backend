c4671f4ced4ae7a4928dc6a70532f316b63fbf25
Chao Wang <cwang8023@gmail.com>
2013-12-03 17:40:26 +0800

    add first view for echo user input

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra'
+require 'nokogiri'
 
 get "/" do
   params[:echostr]
 end
 
 post "/" do
+  doc = Nokogiri::XML::Document.parse request.body
+  myAccount = doc.at_css("ToUserName").child.text;
+  userAccount = doc.at_css("FromUserName").child.text;
+  content = doc.at_css("Content").child.text;
+  haml :weixin_text, locals: { myAccount: myAccount, userAccount: userAccount, content: content }
+end
 
+helpers do
+  def cdata content
+    "<![CDATA[#{content}]]>"
+  end
 end
