3b3b798017bb61fd0e2d1615f8723ca3a8ac7693
Chao Wang <cwang8023@gmail.com>
2014-03-27 22:30:12 +0800

    update encrypt key

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,8 +7,8 @@ echo $SEARCH_RESULTS
 CURRENT_VERSION=`echo $SEARCH_RESULTS | tail -1 | sed -n 's/wei-backend (\(.*\).*)/\1/p'`
 echo "Production version: $CURRENT_VERSION"
 if [ "$REPO_VERSION" != "$CURRENT_VERSION" ]; then
-  echo $GEM_KEY > ~/.gem/credentials
-  cat ~/.gem/credentials
+  echo "---" > ~/.gem/credentials
+  echo $GEM_KEY >> ~/.gem/credentials
   gem build wei-backend.gemspec
   chmod 0600 ~/.gem/credentials
   echo "pushing: wei-backend-$REPO_VERSION.gem"
