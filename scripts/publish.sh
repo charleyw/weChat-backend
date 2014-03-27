79f815a87fac8766c866621fe040a33bb4c14d12
Chao Wang <cwang8023@gmail.com>
2014-03-27 21:13:52 +0800

    update publish shell script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,11 +4,12 @@ REPO_VERSION=`git tag | tail -1`
 echo "Development version: $REPO_VERSION"
 SEARCH_RESULTS=`gem search --remote wei-backend`
 echo $SEARCH_RESULTS
-CURRENT_VERSION=`echo $SEARCH_RESULTS | tail -1 | sed -n 's/wei-backend (\(.*\) .*)/\1/p'`
+CURRENT_VERSION=`echo $SEARCH_RESULTS | tail -1 | sed -n 's/wei-backend (\(.*\).*)/\1/p'`
 echo "Production version: $CURRENT_VERSION"
 if [ "$REPO_VERSION" != "$CURRENT_VERSION" ]; then
-  gem build wei-backend.gemspec
   echo $GEM_KEY > ~/.gem/credentials
+  cat ~/.gem/credentials
+  gem build wei-backend.gemspec
   chmod 0600 ~/.gem/credentials
   echo "pushing: wei-backend-$REPO_VERSION.gem"
   gem push wei-backend-$REPO_VERSION.gem
